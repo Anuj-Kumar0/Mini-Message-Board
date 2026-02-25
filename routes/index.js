aae9ffb44265a69790a34d91c22d3ebb4fbf4ad2
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-25 17:05:22 +0530

    Querying with database

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,40 @@
 const { Router } = require("express");
 const router = Router();
+const pool = require("../db/pool");
 
-const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ];
-  
-  router.get('/', (req, res) => {
-    res.render('index', { messages: messages });
-  });
+router.get('/', async (req, res) => {
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM messages ORDER BY added DESC"
+    );
+    res.render('index', { messages: rows });
+  } catch (err) {
+    console.error(err);
+    res.status(500).render('errors/500');
+  }
+});
+router.get('/message/:id', async(req, res) => {
+  const id = parseInt(req.params.id);
+  const { rows } = await pool.query(
+    "SELECT * FROM messages WHERE id = $1",
+    [id]
+  );
 
-  router.get('/message/:index', (req, res) => {
-    const i = parseInt(req.params.index);
-    const message = messages[i];
-  
-    if (!message) {
-      return res.status(404).render('errors/404');
-    }
-  
-    res.render('messageDetails', { message });
-  });
+  if (!rows[0]) {
+    return res.status(404).render('errors/404');
+  }
 
-  router.post('/new', (req, res) => {
+  res.render('messageDetails', { message: rows[0] });
+});
 
-    const messageUser = req.body.user; 
-    const messageText = req.body.text;
+  router.post('/new', async(req, res) => {
 
-    messages.push({ text: messageText, user: messageUser, added: new Date() });
+   const { username, text } = req.body;
+   
+   await pool.query(
+    "INSERT INTO messages (text, username) VALUES ($1, $2)",
+    [text, username]
+  );
 
     res.redirect("/")
   })
